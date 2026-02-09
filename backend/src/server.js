f76b5551660de62f9441aa2d4d7f9c6a5f7bbe67
Ayushjungari <ayushjungari@gmail.com>
2026-02-09 16:39:00 +0530

    added render-build script

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,17 @@
 import express from "express";
 import path from "path";
+import cors from "cors";
+import {serve} from "inngest/express";
 import {ENV} from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
 const app = express();
 const __dirname = path.resolve();
+
+//middleware
+app.use(express.json());
+//CREDENTILAS: true -> allows cookies to be sent in cross-origin requests
+app.use(cors({origin:ENV.CLIENT_URL, credentials:true}))
+app.use("/api/inngest",serve({client: inngest, functions}))
 app.get("/health", (req,res) => {
     res.status(200).json({msg: "api is up & running"});
 });
@@ -14,7 +22,7 @@ app.get("/books", (req,res) => {
 
 //make our app ready for deployment
 if(ENV.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.use(express.static(path.join(__dirname, "../../frontend/dist")));
     app.get("/{*any}", (req,res) => {
         res.sendFile(path.join(__dirname, "../frontend","dist","index.html"));
         });
